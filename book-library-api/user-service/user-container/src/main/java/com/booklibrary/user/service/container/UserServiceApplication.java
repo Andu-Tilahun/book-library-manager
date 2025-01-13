1c4967a14a8a3f8afd332476c256ae59a8930ec7
Hanan <hanantemam000@gmail.com>
2025-01-13 23:00:05 +0300

    initalize book-library-api

---
+++ book-library-api/user-service/user-container/src/main/java/com/booklibrary/user/service/container/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.booklibrary.user.service.container;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.booklibrary")
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
+
+
