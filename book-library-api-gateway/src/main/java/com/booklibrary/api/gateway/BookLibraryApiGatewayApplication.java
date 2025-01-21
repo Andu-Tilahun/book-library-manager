c4c665e68f489c2d13e7b7b9aab1680aa0acf640
andualem <hanantemam000@gmail.com>
2025-01-21 09:02:25 +0300

    book library api gateway added

---
+++ book-library-api-gateway/src/main/java/com/booklibrary/api/gateway/BookLibraryApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.booklibrary.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLibraryApiGatewayApplication.class, args);
+	}
+
+}
---
+++ book-library-api-gateway/src/test/java/com/booklibrary/api/gateway/BookLibraryApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booklibrary.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
