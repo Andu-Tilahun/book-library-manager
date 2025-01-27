082b52268d110aa33b955166ff71f98bb25a2e2d
andualem <hanantemam000@gmail.com>
2025-01-27 16:56:32 +0300

    unit test added in book service

--- book-library-api/book-service/book-data/src/test/java/com/booklibrary/book/data/book/BookDomainApplicationTest.java
+++ book-library-api/book-service/book-data/src/test/java/com/booklibrary/book/data/book/BookDomainApplicationTest.java
@@ -3,6 +3,7 @@ package com.booklibrary.book.data.book;
 import com.booklibrary.book.data.BookTestConfiguration;
 import com.booklibrary.book.service.data.dto.BookDto;
 import com.booklibrary.book.service.data.entity.BookEntity;
+import com.booklibrary.book.service.data.exception.BookDomainException;
 import com.booklibrary.book.service.data.repository.BookRepository;
 import com.booklibrary.book.service.data.service.BookService;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,9 +14,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(classes = BookTestConfiguration.class)
@@ -30,9 +35,20 @@ public class BookDomainApplicationTest {
 
     private BookEntity book;
 
+    private String isbn = "97817891325874";
+
     @BeforeAll
     public void init() {
 
+        book = BookEntity.builder()
+                .id(UUID.randomUUID())
+                .title("Spring: Microservices with Spring Boot: Build and deploy microservices with Spring Boot")
+                .author("Ranga Rao Karanam")
+                .isbn("97817891325874")
+                .publisher("Packt Publishing")
+                .build();
+
+        when(bookRepository.save(any(BookEntity.class))).thenReturn(book);
 
     }
 
@@ -44,14 +60,33 @@ public class BookDomainApplicationTest {
                         .id(UUID.randomUUID())
                         .title("Spring: Microservices with Spring Boot: Build and deploy microservices with Spring Boot")
                         .author("Ranga Rao Karanam")
-                        .isbn("97817891325874")
+                        .isbn("9781789132587466")
                         .publisher("Packt Publishing")
                         .build();
 
         BookDto result =
                 bookService.createBook(bookDto);
 
-        assertNotNull(result.getCreatedDate());
+        assertNotNull(result);
+
+    }
+
+    @Test
+    public void willThrowErrorWithDuplicateBookIsbn() {
+
+        when(bookRepository.findByIsbn(isbn)).thenReturn(Optional.of(book));
+        BookDto bookDto =
+                BookDto.builder()
+                        .id(UUID.randomUUID())
+                        .title("Spring: Microservices with Spring Boot: Build and deploy microservices with Spring Boot")
+                        .author("Ranga Rao Karanam")
+                        .isbn("97817891325874")
+                        .publisher("Packt Publishing")
+                        .build();
+
+        assertThatThrownBy(() -> bookService.createBook(bookDto))
+                .isInstanceOf(BookDomainException.class)
+                .hasMessageContaining(String.format("Book is already in exist with this isbn: %s", isbn));
 
     }
 }
